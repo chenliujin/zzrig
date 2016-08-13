a37adc5eff32c26d11316520e503740f32338d5f
chenliujin <liujin.chen@qq.com>
2016-08-14 03:13:28 +0800

    add

---
+++ model/product.php
@@ -0,0 +1,17 @@
+<?php
+
+class product extends Model
+{
+	/**
+	 * @author chenliujin <liujin.chen@qq.com>
+	 * @since 2016-08-22
+	 */
+	static public function GetTableName()
+	{
+		return 'product';
+	}
+
+	public function getPrimaryKey()
+	{
+	}
+}
---
+++ model/product_description_en.php
@@ -0,0 +1,17 @@
+<?php
+
+class product_description extends Model
+{
+	/**
+	 * @author chenliujin <liujin.chen@qq.com>
+	 * @since 2016-08-22
+	 */
+	static public function GetTableName()
+	{
+		return 'product_description';
+	}
+
+	public function getPrimaryKey()
+	{
+	}
+}
