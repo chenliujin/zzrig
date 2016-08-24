c423ab83b5ed0f61a6d42be0812de75935cf5f85
chenliujin <liujin.chen@qq.com>
2016-08-24 19:15:03 +0800

    update

---
+++ application/checkout/controller/Index.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace app\checkout\controller;
+
+use \think\Controller;
+
+class Index extends Controller
+{
+	/**
+	 * @author chenliujin <liujin.chen@qq.com>
+	 * @since 2016-08-24
+	 */
+	public function Index()
+	{
+		echo 'ok';
+	}
+}
---
+++ application/express_checkout/controller/Index.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace app\express_checkout\controller;
+
+use \think\Controller;
+
+class Index extends Controller 
+{
+	/**
+	 * @author chenliujin <liujin.chen@qq.com>
+	 * @since 2016-08-24
+	 */
+	public function Index()
+	{
+	}
+}
